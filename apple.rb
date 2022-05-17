3f5216e060443292f9b27d3502de9cce98a43d54
mohammadzubair20 <mohammadzubairprabha123>
2022-05-17 19:33:38 +0530

    rb.files

--- apple.rb
+++ apple.rb
@@ -1,22 +1,24 @@
-def method1(x=9, y=6, z)
-	puts x
-	puts y
-	puts z
+#def method1(x=9, y=6, z)
+#	puts x
+#	puts y
+#	puts z
 
 
-end
-
-#def method2(x*)
-#	puts x.class
-
-#	x.each do |ele|
-#		puts ele *2
-
- # end
-  
 #end
 
-  # method1(8)
-   method1(10)
+def method2(*x)
+	puts x.class
 
-   #method2(3,7,8,4,9)
+	x.each do |ele|
+		 ele *2
+  #return x #last
+  puts "hfsggssh"
+  puts "hfsggs" if 3==6
+  end
+  
+end
+
+  # method1(8)
+  # method1(10)
+
+   # puts method2(3,7,8,4,9)
