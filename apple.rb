c1e0ddcba37ef91437e258a7b7d021df7424cf37
mohammadzubair20 <mohammadzubairprabha123>
2022-05-17 19:15:18 +0530

    . rb files

---
+++ apple.rb
@@ -0,0 +1,22 @@
+def method1(x=9, y=6, z)
+	puts x
+	puts y
+	puts z
+
+
+end
+
+#def method2(x*)
+#	puts x.class
+
+#	x.each do |ele|
+#		puts ele *2
+
+ # end
+  
+#end
+
+  # method1(8)
+   method1(10)
+
+   #method2(3,7,8,4,9)
