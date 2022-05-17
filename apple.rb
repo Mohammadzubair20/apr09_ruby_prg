952c537e71459858b66249e7f113a56dd279d373
mohammadzubair20 <mohammadzubairprabha123>
2022-05-18 02:10:51 +0530

    rb files

--- apple.rb
+++ apple.rb
@@ -1,24 +1,47 @@
-#def method1(x=9, y=6, z)
-#	puts x
-#	puts y
-#	puts z
+# # #def method1(x=9, y=6, z)
+# # #	puts x
+# # #	puts y
+# # #	puts z
 
 
-#end
+# # #end
 
-def method2(*x)
-	puts x.class
+# # #def method2(*x)
+# # #	puts x.class
 
-	x.each do |ele|
-		 ele *2
-  #return x #last
-  puts "hfsggssh"
-  puts "hfsggs" if 3==6
-  end
+# # #	x.each do |ele|
+# # #		 ele *2
+# #   #return x #last
+# #  # puts "hfsggssh"
+# #   #puts "hfsggs" if 3==6
+# #   #end
   
-end
+# # #end
 
-  # method1(8)
-  # method1(10)
+# #   # method1(8)
+# #   # method1(10)
 
-   # puts method2(3,7,8,4,9)
+# #  #  puts method2(3,7,8,4,9)
+
+ 
+#   # x = [6,7,8,5,35]
+#     #puts x.sort
+
+#  #x.sort! { |first_num, second_num| first_num <=> second_num } #decr
+#  #x.sort! { |first_num, second_num| second_num <=> first_num } #inc
+#    #puts x.sort.reverse
+
+
+#  f =  def method1
+#     puts "cat is a animal"
+#   end
+
+    
+#     puts f
+#     puts f
+
+
+#sunny = Proc.new { |m| puts m**2 }
+  [7,2,6] .each(&sunny)
+
+Proc .new
